f40bbd0e3548d61c82077459926534f01ecfe5fa
timur anoshechkin <tima@quests-end.com>
2015-04-25 20:02:38 +1000

    - unpacking scene to be used by UI element

--- Sandplay5.0/Sandplay Android Version/Prototype/Assets/Scripts/LoadScenes.cs
+++ Sandplay5.0/Sandplay Android Version/Prototype/Assets/Scripts/LoadScenes.cs
@@ -9,15 +9,27 @@ using System.Linq;
 public class LoadScenes : MonoBehaviour
 {
     private List<SaveEntry> saveEntries = null;
+    public struct SaveEntryPair
+    {
+        public SaveEntry SaveEntry;
+        public Texture2D Texture;
+    }
+
+    private List<SaveEntryPair> entryPairs = null;
+    public List<SaveEntryPair> SaveEntries { get { return entryPairs; } }
 
     // Use this for initialization
     void Start()
     {
-        // get names of the save files
+        // get save files
         saveEntries = DeserializeSaveEntries( GetSaveGameNames() );
+        entryPairs = new List<SaveEntryPair>();
+        // populate SaveEntry pairs with save entry and an image
         foreach ( SaveEntry se in saveEntries )
         {
-            Debug.Log( se._screenshot_filename );
+            SaveEntryPair pair = new SaveEntryPair();
+            pair.SaveEntry = se;
+            pair.Texture = GetTexture( se );
         }
     }
 
@@ -53,5 +65,61 @@ public class LoadScenes : MonoBehaviour
         }
 
         return saveEntries;
+    }
+
+    private Texture2D GetTexture( SaveEntry entry )
+    {
+        Texture2D texture = null;
+        //Open the stream and read it back. 
+        using ( FileStream fs = File.OpenRead( entry._screenshot_filename ) )
+        {
+            byte[] imageData = new byte[ fs.Length ];
+            fs.Read( imageData, 0, ( int )fs.Length );
+
+            // getting texture size before loading texture
+            float width;
+            float height;
+            GetPNGTextureSize( imageData, out width, out height );
+
+            texture = new Texture2D( (int)width, (int)height );
+            texture.LoadImage( imageData );
+        }
+        return texture;
+    }
+
+
+    /// <summary>
+    /// Not mine pasted from http://denis-potapenko.blogspot.com.au/2013/10/task-7-getting-size-of-png-textures.html
+    /// Checks the size of the texture before it is being loaded
+    /// </summary>
+    private void GetPNGTextureSize( byte[] bytes, out float width, out float height )
+    {
+        width = -1.0f;
+        height = -1.0f;
+
+        // check only png tex!!! // http://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html
+        byte[] png_signature = { 137, 80, 78, 71, 13, 10, 26, 10 };
+
+        const int cMinDownloadedBytes = 30;
+        byte[] buf = bytes;
+        if ( buf.Length > cMinDownloadedBytes )
+        {
+            // now we can check png format
+            for ( int i = 0; i < png_signature.Length; i++ )
+            {
+                if ( buf[ i ] != png_signature[ i ] )
+                {
+                    Debug.LogWarning( "Error! Texture os NOT png format!" );
+                    return; // this is NOT png file!
+                }
+            }
+
+            // now get width and height of texture
+            width = buf[ 16 ] << 24 | buf[ 17 ] << 16 | buf[ 18 ] << 8 | buf[ 19 ];
+            height = buf[ 20 ] << 24 | buf[ 21 ] << 16 | buf[ 22 ] << 8 | buf[ 23 ];
+
+            Debug.Log( "Loaded texture size: width = " + width + "; height = " + height );
+            return;
+        }
     }
 }
