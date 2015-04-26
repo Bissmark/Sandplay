db4ca12be3debc2ba2884a9b93e09fb6f6e69ac9
timur anoshechkin <tima@quests-end.com>
2015-04-26 18:41:02 +1000

    - sound blends in and out.

--- Sandplay5.0/Sandplay Android Version/Prototype/Assets/Scripts/SoundChange.cs
+++ Sandplay5.0/Sandplay Android Version/Prototype/Assets/Scripts/SoundChange.cs
@@ -6,24 +6,45 @@ public class SoundChange : MonoBehaviour
 	public GameObject soundEnabledIcon;
 	public GameObject soundDisabledIcon;
 	public AudioSource music;
+
+    [SerializeField]
+    public float muteRatio = 0.01f;
+
     private float volume = 0;
-	
+    private float initialVolume = 0;
+    private bool mute = false;
+
+
+    private void Awake()
+    {
+        initialVolume = music.volume;
+    }
+
 	public void SoundMute() 
 	{
 		if(soundEnabledIcon.activeSelf == false)
 		{
 			soundEnabledIcon.SetActive(true);
 			soundDisabledIcon.SetActive(false);
-            music.volume = volume;
-			//music.mute = false;
+            mute = false;
 		}
 		else
 		{
 			soundEnabledIcon.SetActive(false);
 			soundDisabledIcon.SetActive(true);
-            volume = music.volume;
-            music.volume = 0;
-            //music.mute = true;
+            mute = true;
 		}
 	}
+
+    private void Update()
+    {
+        if ( mute )
+        {
+            music.volume = Mathf.Clamp(Mathf.Lerp( music.volume, 0f, muteRatio), 0, 1 );
+        }
+        else
+        {
+            music.volume = Mathf.Clamp(Mathf.Lerp( music.volume, 1.0f, muteRatio ), 0, 1);
+        }
+    }
 }
